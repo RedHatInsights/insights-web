2c8138c28b3399a108752189c20fd6bc0f267090
Kyle Lape <klape@redhat.com>
2017-07-06 19:56:05 -0500

    Add boto3 as dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
         packages=["insights_web"],
         install_requires=[
             'logstash_formatter',
+            'boto3',
             'insights-core',
             'flask==0.12.1',
             'uWSGI==2.0.15'
