7ddc3214506f8ab06cf732b3e880f84a2e0e5500
Jesse Jaggars <jjaggars@redhat.com>
2017-10-06 12:11:43 -0400

    tweaking log format for exceptions

--- insights_web/util.py
+++ insights_web/util.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import traceback
 from flask import json
 from logstash_formatter import LogstashFormatterV1
 from insights.settings import web as config
@@ -13,6 +14,12 @@ class OurFormatter(LogstashFormatterV1):
     def format(self, record):
         if hasattr(thread_context, "request_id"):
             setattr(record, "request_id", thread_context.request_id)
+
+        exc = getattr(record, "exc_info")
+        if exc:
+            setattr(record, "exception", "".join(traceback.format_exception(*exc)))
+            setattr(record, "exc_info", None)
+
         return super(OurFormatter, self).format(record)
 
 
